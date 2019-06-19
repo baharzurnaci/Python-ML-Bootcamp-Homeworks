01f2df51455fad690e626d0d61c038b2cd74cf02
baharzurnacı <51044288+baharzurnaci@users.noreply.github.com>
2019-06-20 00:28:52 +0300

    Create MusteriVerileriniListeyeYazma.py

---
+++ MusteriVerileriniListeyeYazma.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+liste = []
+Ad =        input( "Ad:" )
+Soyad =     input( " Soyad: ")
+DogumYili = int( input(" DogumYili: "))
+
+
+for i in liste:
+  print(i)
+
